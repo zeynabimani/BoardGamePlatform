620681edaa6babc144c9dcb9909546b42155e7cb
fbghkh1999 <40722872+fbghkh1999@users.noreply.github.com>
2021-02-04 18:55:01 +0330

    start waiting room

--- www/sadaf/waitingRoom.php
+++ www/sadaf/waitingRoom.php
@@ -1,55 +1,68 @@
-<?php
-include "header.inc.php";
-HTMLBegin();
+    <?php
+    include "header.inc.php";
+    HTMLBegin();
 
-if(isset($_REQUEST["EnterWaitingRoom"])){
-    $mysql = pdodb::getInstance();
-    $query = "select * from sadaf.room where roomID < 100";
-    $res = $mysql->Execute($query);
-    while($rec = $res->fetch()){
-        $ChGameID = "ch_" . $rec["roomID"]; 
-        if(isset($_REQUEST[$ChGameID]) && $rec["managerID"] == ""){            
-            $query = "update sadaf.room set managerID = " . $_SESSION["PersonID"] . " where roomID= " . $rec["roomID"];
-            $res = $mysql->Execute($query);
-
-            $query2 = "insert into sadaf.game (roomID, userID) values (?,?)";
-            $mysql->Prepare($query2);
-            $mysql->ExecuteStatement(array($rec["roomID"], $_SESSION["PersonID"]));
+    ?>
+    <?php
+    function getWaitingPeople(){
+        $results=array();
+        $game=array();
+        $persons=array();
+        $i=0;
+        $j=0;
+        $t=0;
+        $is_recognize=false;
+        $mysql = pdodb::getInstance();
+        $query = "select * from sadaf.accountspecs";
+        $query2 = "select * from sadaf.game";
+        $res = $mysql->Execute($query);
+        $res2 = $mysql->Execute($query2);
+        while($rec = $res->fetch()){
+         $persons[$i]=array($rec["UserID"],$rec["PersonID"]);
+         $i=$i+1;
         }
-    }
-}
-?>
-<?php
-function getWaitingPeople(){
-    $results=array();
-    $i=0;
-    $mysql = pdodb::getInstance();
-    $query = "select * from sadaf.accountspecs";
-    $query2 = "select * from sadaf.game";
-    $res = $mysql->Execute($query);
-    $res2 = $mysql->Execute($query2);
-    while($rec = $res->fetch()){
+
         while($rec2 = $res2->fetch()){
-            if($rec[PersonID]===$rec2[userID]){
-                $results[$i]=array($rec[UserID],$rec[PersonID]);
-                $i=$i+1;
-            }
-
+            $game[$j]=$rec2["userID"];
+            $j=$j+1;
         }
-       
+        for($p=0;$p<count($persons);$p++){
+            $is_recognize=false;
+            for($g=0;$g<count($game)&&$is_recognize==false;$g++){
+                if($persons[$p][1]===$game[$g]){
+                    $results[$t]=array($persons[$p][0],$persons[$p][1]);
+                    $is_recognize=true;
+                    $t=$t+1;
+                   
+                }
+            }
+        }
+              
+        
+        
+    return $results; 
     }
-  return $results; 
-}
-?>
-<form method="POST">
-    <input type="hidden" name="EnterWaitingRoom" value="1">
-    <table class="table table-sm table-bordered table-striped">
-        <tr>
-            <th>نام کاربر</th> 
-            <th>شماره کاربر</th>   
-        </tr>
-</form>
-<?php
-    $mysql = pdodb::getInstance();
-    $query = "select PersonID from sadaf.accountspecs";
-    $res = $mysql->Execute($query);
+    ?>
+    <form method="POST">
+        <input type="hidden" name="EnterWaitingRoom" value="1">
+        <table class="table table-sm table-bordered table-striped">
+            <tr>
+                <th>نام کاربر</th> 
+                <th>شماره کاربر</th>   
+            </tr>
+    <?php
+        $Peoples =getWaitingPeople();
+        for($i=0;$i<count($Peoples);$i++){
+            echo "<tr>
+            <td>".$Peoples[$i][0]."</td>
+            <td>".$Peoples[$i][1]."</td>
+            
+            </tr>";
+        }
+        
+    ?>
+    </table>
+    </form>
+
+    </body>
+    </html>
