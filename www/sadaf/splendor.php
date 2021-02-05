610d4d1b98ecedd9719b25848f99f390e00fc0ea
fbghkh1999 <40722872+fbghkh1999@users.noreply.github.com>
2021-02-05 22:55:14 +0330

    add invitatio button in game

--- www/sadaf/splendor.php
+++ www/sadaf/splendor.php
@@ -192,6 +192,167 @@ function function3($array, $tu)
 }
 
 ?>
+<?php
+     function isRoomManager(){
+        $isManager=0;
+        $mysql = pdodb::getInstance();
+        $query = "select * from sadaf.room where roomID = " . $_SESSION["id"];
+        $res = $mysql->Execute($query); 
+       while($rec = $res->fetch()){
+           if($_SESSION["PersonID"]===$rec["managerID"]){
+            
+              $isManager=1;
+               
+    
+          }
+       }
+        
+        return $isManager;
+    }
+    function getPersons(){
+        $results= array();
+        $persons=array();
+        $i=0;
+        $mysql = pdodb::getInstance();
+        $query = "select * from sadaf.game where roomID = " . $_SESSION["id"];
+        $query2 = "select * from sadaf.accountspecs";
+        $res2 = $mysql->Execute($query2); 
+        while($rec2 = $res2->fetch()){
+            $persons[$i]=array($rec2["PersonID"],$rec2["UserID"]);
+            $i=$i+1;
+
+        }
+         $copypersons=$persons;
+        $res = $mysql->Execute($query); 
+       while($rec = $res->fetch()){
+        for($j=0;$j<count($persons);$j++){
+            $is_recognize=false;
+           if($persons[$j][0]===$rec["userID"]&& $is_recognize==false){
+            unset($copypersons[$j]);
+            $is_recognize=true;
+               
+    
+          }
+       }
+    }
+    $copypersons=array_values($copypersons);
+    $results=$copypersons;
+    $stOptions="";
+    for($p=0;$p<count($results);$p++){
+        $stOptions.="<option value='".$results[$p][1]."'>";
+        $stOptions.=$results[$p][1];
+        $stOptions.="</option>";
+
+    }
+    return $stOptions;
+    }
+
+
+    ?>
+    <?php
+     function addtoDataBase($v){
+        $isManager=0;
+        $i=0;
+        $persons=array();
+        $mysql = pdodb::getInstance();
+        $query2 = "select * from sadaf.accountspecs";
+        $res2 = $mysql->Execute($query2); 
+        while($rec2 = $res2->fetch()){
+            $persons[$i]=array($rec2["PersonID"],$rec2["UserID"]);
+            $i=$i+1;
+
+        }
+        for($s=0;$s<count($persons);$s++){
+            if($persons[$s][1]===$v){
+                $idNew=$persons[$s][0];
+            }
+        }
+        $query3="select * from sadaf.game_request";
+        $res3 = $mysql->Execute($query3); 
+        while($rec3 = $res3->fetch()){
+            if(($rec3["roomID"]!=$_SESSION["id"])&&($rec3["userID"]!=$idNew)){
+                $query = "insert ignore into sadaf.game_request (roomID, userID, status)
+                values (".$_SESSION["id"].",".$idNew.", 'Waiting');";
+                $mysql->Execute($query); 
+            }
+           
+        }
+        
+     
+        
+    }
+    
+      
+    ?>
+    <?php
+    if(isset($_REQUEST["Users"])&&isset($_REQUEST["ersal"])){
+        addtoDataBase($_REQUEST["Users"]);
+    }
+    
+    ?>
+
+
+<form method="POST" >
+<div class="container">
+  
+  <!-- Trigger the modal with a button -->
+  <?php
+  $isManager=isRoomManager();
+  if($isManager===1){
+     
+     echo " <button type=\"button\" class=\"btn btn-success btn-sm\" data-toggle=\"modal\" data-target=\"#myModal\">Send Invitation to your Freinds</button>" ;
+  }
+  elseif($isManager===0){
+     
+      $disabled="disabled";
+    echo " <button type=\"button\" class=\"btn btn-success btn-sm\"".$disabled.">Send Invitation to your Freinds</button>" ;
+
+  }
+  ?>
+
+<div class="modal fade" id="myModal" role="dialog" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+    
+      <!-- Modal content-->
+      <div class="modal-content">
+        <div class="modal-header align-right DivRtl">
+         <h4 class="modal-title ">ارسال دعوت نامه</h4>
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+          
+        </div>
+        <div class="modal-body">
+        <div class="row">
+		<div class="col">
+          <p> انتخاب لیست کاربران</p>
+          <hr>
+          <table class="table table-sm table-stripped table-bordered">
+          <tr>
+					
+			<td>
+            <select class="form-control sadaf-m-input" name="Users" id="Persons">
+            <option value=0>-
+            <? echo getPersons();?>
+           
+        </td>
+        <td>
+        <input type="submit" name="ersal" class="btn btn-primary" value="Send">
+        </td>
+        </tr>
+        </select>
+          </table>
+        </div>
+        </div>
+        </div>
+        <div class="modal-footer DivRtl">
+          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        </div>
+      </div>
+      
+    </div>
+  </div>          
+  
+</div>   
+</form>
 
 <script src="JsFile.js"></script>
 
