65a5ac738cd92858dbca60c04acc5866f2250706
mohadeseh <mohadeseh_jabbari@yahoo.com>
2021-02-05 17:43:39 +0330

    splendor2

--- www/sadaf/Token.php
+++ www/sadaf/Token.php
@@ -1,6 +1,3 @@
-<?php
-
-
 class Token
 {
     public function __construct()
--- www/sadaf/splendor.php
+++ www/sadaf/splendor.php
@@ -50,7 +50,7 @@ if(array_key_exists('select1', $_POST)){
 
 function function1($array)
 {
-    if(isset($_POST['red'])){
+    if(isset($_POST['orange'])){
         echo $array[0];
     }
 }
@@ -63,9 +63,9 @@ function function1($array)
     <input type="hidden" name="EnterRoom" value="1">
     <table class="table table-sm table-bordered table-striped">
         <tr>
-            <td>انتخاب 3 توکن از 3 رنگ مختلف</td>
-            <td>انتخاب 2 توکن همرنگ</td>
-            <td>رزرو یک کارت پیشرفت و دریافت ۱ توکن طلا</td>
+            <td>انتخاب ۳ الماس </td>
+            <td>انتخاب ۲ الماس همرنگ</td>
+            <td>رزرو یک کارت پیشرفت و دریافت ۱ الماس طلا</td>
             <td>خرید یکی از کارت‌های وسط میز</td>
             <td>خرید یکی از کارت‌هایی که رزرو کرده</td>
         </tr>
@@ -73,54 +73,102 @@ function function1($array)
             <td>
                 <table >
                     <tr>
-                        <td>red token</td>
-                        <td><input type="checkbox" name="red" onclick="select1(this.name)"></td>
+                        <td>orange</td>
+                        <td><img id= "img_fraction" style="width:2vw;" src="images/tokens/orange2.png" ></td>
+                        <td><input type="checkbox" name="orange" onclick="select1(this.name)"></td>
                     </tr>
                     <tr>
-                        <td>blue token</td>
+                        <td>blue</td>
+                        <td><img id= "img_fraction" style="width:2vw;" src="images/tokens/blue2.png" ></td>
                         <td><input type="checkbox" name="blue" onclick="select1(this.name)"></td>
                     </tr>
                     <tr>
-                        <td>green token</td>
+                        <td>green</td>
+                        <td><img id= "img_fraction" style="width:2vw;" src="images/tokens/green2.png" ></td>
                         <td><input type="checkbox" name="green"onclick="select1(this.name)"></td>
                     </tr>
                     <tr>
-                        <td>brown token</td>
-                        <td><input type="checkbox" name="brown" onclick="select1(this.name)"></td>
+                        <td>dark blue</td>
+                        <td><img id= "img_fraction" style="width:2vw;" src="images/tokens/dark_blue2.png" ></td>
+                        <td><input type="checkbox" name="darkBlue" onclick="select1(this.name)"></td>
                     </tr>
                     <tr>
-                        <td>white token</td>
-                        <td><input type="checkbox"></td>
+                        <td>pink</td>
+                        <td><img id= "img_fraction" style="width:2vw;" src="images/tokens/pink2.png" ></td>
+                        <td><input type="checkbox" name="pink" onclick="select1(this.name)"></td>
                     </tr>
                 </table>
             </td>
             <td>
                 <table >
                     <tr>
-                        <td>red token</td>
-                        <td><input type="checkbox"></td>
+                        <td>orange</td>
+                        <td><img id= "img_fraction" style="width:2vw;" src="images/tokens/orange2.png" ></td>
+                        <td><img id= "img_fraction" style="width:2vw;" src="images/tokens/orange2.png" ></td>
+                        <td><input type="checkbox" name="orange" onclick="select1(this.name)"></td>
                     </tr>
                     <tr>
-                        <td>blue token</td>
-                        <td><input type="checkbox"></td>
+                        <td>blue</td>
+                        <td><img id= "img_fraction" style="width:2vw;" src="images/tokens/blue2.png" ></td>
+                        <td><img id= "img_fraction" style="width:2vw;" src="images/tokens/blue2.png" ></td>
+                        <td><input type="checkbox" name="blue" onclick="select1(this.name)"></td>
                     </tr>
                     <tr>
-                        <td>green token</td>
-                        <td><input type="checkbox"></td>
+                        <td>green</td>
+                        <td><img id= "img_fraction" style="width:2vw;" src="images/tokens/green2.png" ></td>
+                        <td><img id= "img_fraction" style="width:2vw;" src="images/tokens/green2.png" ></td>
+                        <td><input type="checkbox" name="green"onclick="select1(this.name)"></td>
                     </tr>
                     <tr>
-                        <td>brown token</td>
-                        <td><input type="checkbox"></td>
+                        <td>dark blue</td>
+                        <td><img id= "img_fraction" style="width:2vw;" src="images/tokens/dark_blue2.png" ></td>
+                        <td><img id= "img_fraction" style="width:2vw;" src="images/tokens/dark_blue2.png" ></td>
+                        <td><input type="checkbox" name="darkBlue" onclick="select1(this.name)"></td>
                     </tr>
                     <tr>
-                        <td>white token</td>
-                        <td><input type="checkbox"></td>
+                        <td>pink</td>
+                        <td><img id= "img_fraction" style="width:2vw;" src="images/tokens/pink2.png" ></td>
+                        <td><img id= "img_fraction" style="width:2vw;" src="images/tokens/pink2.png" ></td>
+                        <td><input type="checkbox" name="pink" onclick="select1(this.name)"></td>
                     </tr>
                 </table>
             </td>
-            <td></td>
-            <td></td>
-            <td></td>
+            <td>
+                <table>
+                    <tr>
+                        <td>
+                            reserve one card
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <img id= "img_fraction" style="width:5vw;" src="images/tokens/gold2.png" >
+                        </td>
+                    </tr>
+                </table>
+            </td>
+
+            <td>
+                <table>
+                    <tr>
+                        <td>
+                            buy one card
+                        </td>
+                    </tr>
+
+                </table>
+            </td>
+
+            <td>
+                <table>
+                    <tr>
+                        <td>
+                            buy reserved card
+                        </td>
+                    </tr>
+
+                </table>
+            </td>
         </tr>
         <tr>
             <td><button name="select1">انتخاب 1</button></td>
@@ -128,6 +176,36 @@ function function1($array)
             <td><button>انتخاب 3</button></td>
             <td><button>انتخاب 4</button></td>
             <td><button>انتخاب 5</button></td>
+        </tr>
+        <tr>
+            <td><img id= "img_fraction" style="width:10vw;" src="images/cards/1.png" ></td>
+            <td><img id= "img_fraction" style="width:10vw;" src="images/cards/2.png" ></td>
+            <td><img id= "img_fraction" style="width:10vw;" src="images/cards/3.png" ></td>
+            <td><img id= "img_fraction" style="width:10vw;" src="images/cards/4.png" ></td>
+        </tr>
+        <tr>
+            <td><img id= "img_fraction" style="width:10vw;" src="images/cards/5.png" ></td>
+            <td><img id= "img_fraction" style="width:10vw;" src="images/cards/6.png" ></td>
+            <td><img id= "img_fraction" style="width:10vw;" src="images/cards/7.png" ></td>
+            <td><img id= "img_fraction" style="width:10vw;" src="images/cards/8.png" ></td>
+        </tr>
+        <tr>
+            <td><img id= "img_fraction" style="width:10vw;" src="images/cards/9.png" ></td>
+            <td><img id= "img_fraction" style="width:10vw;" src="images/cards/10.png" ></td>
+            <td><img id= "img_fraction" style="width:10vw;" src="images/cards/11.png" ></td>
+            <td><img id= "img_fraction" style="width:10vw;" src="images/cards/12.png" ></td>
+        </tr>
+        <tr>
+            <td><img id= "img_fraction" style="width:10vw;" src="images/cards/13.png" ></td>
+            <td><img id= "img_fraction" style="width:10vw;" src="images/cards/14.png" ></td>
+            <td><img id= "img_fraction" style="width:10vw;" src="images/cards/15.png" ></td>
+            <td><img id= "img_fraction" style="width:10vw;" src="images/cards/16.png" ></td>
+        </tr>
+        <tr>
+            <td><img id= "img_fraction" style="width:10vw;" src="images/cards/17.png" ></td>
+            <td><img id= "img_fraction" style="width:10vw;" src="images/cards/18.png" ></td>
+            <td><img id= "img_fraction" style="width:10vw;" src="images/cards/19.png" ></td>
+            <td><img id= "img_fraction" style="width:10vw;" src="images/cards/20.png" ></td>
         </tr>
         <?php
 
